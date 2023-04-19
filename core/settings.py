43d4e0e04f60b384c954d64fd136feeb5af01644
nurbekusenovvv <unurbek499@gmail.com>
2023-04-19 01:11:11 -0400

    created apps user and transfer

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9#cvg6oz+u0vu+k(el2fq_4yj+&+o63z$2kq&ajdy)=0r)yx3b'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #rest
+    'rest_framework',
+    #apps
+    'apps.user',
+    'apps.transfer',
 ]
 
 MIDDLEWARE = [
